46ed26989a670be99cc88e55da5ec9eecee558d7
Dilan <dunver33@gmail.com>
2021-01-27 22:00:53 +0300

    continue to work on the project

--- app.js
+++ app.js
@@ -10,92 +10,97 @@ const itemHolder = document.querySelector('#item-holder');
 const addBtn = document.querySelector('.add-btn')
 isEditing = false;
 let editElement;
-form.addEventListener('submit', findingAnOption);
+form.addEventListener('submit', chooseAnOption);
 
-function findingAnOption(e){
-  e.preventDefault()
+function chooseAnOption(e){
+  e.preventDefault();
   if(isEditing){
-    addedEdit(e)
-  } else {
+    editedItems(e);
+  }else {
     addItem(e)
   }
 }
-function addedEdit(e){
-e.preventDefault();
+
+
+function editedItems(e){
+  e.preventDefault();
   editElement.name = name.value;
   editElement.date = date.value;
   editElement.amount = amount.value;
-addBtn.textContent ='Add Expense'
-isEditing = false;
-renderItems()
-setBackToDefault()
-}
-
-function addItem(e){
-  e.preventDefault();
-  const idValue = new Date().getTime().toString()
-  const nameValue = name.value;
-  const dateValue = date.value;
-  const amountValue = amount.value;
-  const item = {
-    id: idValue,
-    name : nameValue || '-',
-    date : dateValue || '-',
-    amount : amountValue || '-'
-  }
-  expensesArray.push(item);
+  isEditing = false;
+  addBtn.textContent = 'add expense';
   renderItems()
   setBackToDefault()
 }
 
-function deleteItems(e){
+function addItem(e){
+  e.preventDefault();
+  const idValue = new Date().getTime().toString();
+  const nameValue = name.value || '-';
+  const dateValue = date.value || '-';
+  const amountValue = amount.value || '-';
+  const item = {
+    id: idValue, 
+    name : nameValue,
+    date : dateValue,
+    amount : amountValue
+  }
+  expensesArray.push(item)
+  renderItems()
+  setBackToDefault()
+}
+function deleteItem(e){
   const element = e.currentTarget.parentElement.parentElement;
   const id = element.dataset.id;
-  const elementToDelete = expensesArray.find(item =>{
-    return item.id === id
-  });
-  const indexOfElement = expensesArray.indexOf(elementToDelete);
-  expensesArray.splice(indexOfElement,1);
+  const deleteToElement = expensesArray.find(item =>{
+    return item.id === id;
+  })
+  const indexOfDeletedElement = expensesArray.indexOf(deleteToElement);
+  expensesArray.splice(indexOfDeletedElement,1);
   renderItems()
 }
 function editItems(e){
   const element = e.currentTarget.parentElement.parentElement;
   const id = element.dataset.id;
-  editElement = expensesArray.find(item=>
-    item.id===id);
-    name.value = editElement.name;
-    date.value = editElement.date;
-    amount.value = editElement.amount;
-  isEditing = true; 
-  addBtn.textContent = 'Edit';
-} 
+ editElement =  expensesArray.find(item =>{
+    return item.id == id;
+  });
+   name.value = editElement.name;
+   date.value = editElement.date;
+  amount.value = editElement.amount ;
+  isEditing = true;
+  addBtn.textContent = 'edit'
+  renderItems()
+}
 
 function renderItems(){
-  itemHolder.innerHTML='';
+  itemHolder.innerHTML = ' ';
   expensesArray.forEach(item => {
     const element = document.createElement('div');
-    element.setAttribute('data-id', item.id);
     element.classList.add('list-items');
-    element.innerHTML = ` <p>${item.name}</p>
+    element.setAttribute('data-id', item.id);
+    element.innerHTML = `<p>${item.name}</p>
     <p>${item.date}</p>
     <p>
-      ${item.amount}
+    ${item.amount}
       <button type="button" class= "delete-btn">
         <i class="fas fa-times-circle fa-2x"></i>
       </button>
       <button type="button" class= "edit-btn">
-      <i class="fas fa-edit fa-2x"></i>
-      </button>`
-      itemHolder.appendChild(element);
-      const deleteBtn = element.querySelector('.delete-btn');
-      deleteBtn.addEventListener('click', deleteItems);
-      const editBtn = element.querySelector('.edit-btn');
-      editBtn.addEventListener('click', editItems);
-  });
+        <i class="fas fa-edit fa-2x"></i>
+      </button>
+      </p>`
+    itemHolder.appendChild(element);
+    const deleteBtn = element.querySelector('.delete-btn');
+    const editBtn = element.querySelector('.edit-btn');
+    deleteBtn.addEventListener('click', deleteItem);
+    editBtn.addEventListener('click', editItems)
+  })
+  
 }
-
 function setBackToDefault(){
   name.value = '';
   date.value = '';
   amount.value = '';
 }
+
